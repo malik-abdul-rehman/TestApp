a5442bd6723bab848bf71a0279f654fe1514aba1
malik-abdul-rehman <abdul.rehman@tetrahex.com>
2023-08-01 18:06:41 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React from "react";
-import HumanBody from "./assets/humanBody.jpg";
-
 const APP = () => {
   return (
     <div>
-      <img width={800} src={HumanBody} alt="Human Body" useMap="#bodyMap" />
+      <h1>Hi Malik</h1>
     </div>
   );
 };
